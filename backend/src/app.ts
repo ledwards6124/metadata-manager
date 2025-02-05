92f43c07a20c0293bcf5c34a555826690abe0690
Luke <lce1868@g.rit.edu>
2025-02-05 15:15:10 -0500

    trying to make api calls?

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import { readMP3 } from './io/mp3io';
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
 app.get('/', (req, res) => {
+  console.log('Ping from ' + req.ip)
   res.send('Hello World!');
 });
+
+app.get('/mp3', (req, res) => {
+  const path = req.query.path;
+  const tags = readMP3(path);
+  res.send(tags);
+})
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  useEffect(() => {
-    if (window.Electron) {
-      window.Electron.ipcRenderer.send('page-rendered');
-    }
-  }, []);
+  const [text, setText] = useState('');
+
+  const path = '..\\data\\flowers.mp3'
+
+
+  const call = () => {
+    
+    fetch('http://localhost:5000/mp3?path=' + path, {
+      method: 'GET'
+    }).then(res => {
+      return res.text();
+    }).then(jData => {
+      console.log(jData);
+      setText(jData);
+    })
+  }
 
   return (
     <>
     <h1>Metadata Manager</h1>
+    <button onClick={call}>Click me</button>
+    <p>{text}</p>
     </>
   );
 }
