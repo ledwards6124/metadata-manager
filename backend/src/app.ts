f55ddea3b3517ea62c6fe0a4ff49447b0707e76f
Luke <lce1868@g.rit.edu>
2025-02-05 14:22:22 -0500

    Revert "switching machine"
    This reverts commit 6a2cfb9bd503293a3442cd5f662538133984df79.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,9 @@
 import express from 'express';
-const cors = require('cors');
 const app = express();
 const port = 5000;
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
-
 app.get('/', (req, res) => {
-  res.send(JSON.parse('Hello World!'));
-  console.log('GET from ' + req.headers.origin);
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
-/* eslint-ignore */
-
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
 
@@ -12,25 +10,9 @@ function App() {
     }
   }, []);
 
-  const [text, setText] = useState('');
-
-
-  const call = () => {
-    fetch('http://localhost:5000/', {
-      method: 'GET'
-    }).then((response) => {
-      return response.json();
-    }).then((jData) => {
-      setText(jData);
-    })
-  
-  }
-
   return (
     <>
     <h1>Metadata Manager</h1>
-    <button onClick={call}>Click me</button>
-    <p>{text}</p>
     </>
   );
 }
